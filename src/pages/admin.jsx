a057c7d7160aa302bf375461468027e49e1e947d
Ulil Albab <ulilalbabcmpk@gmail.com>
2024-11-27 17:43:15 +0700

    feat: add spinner

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 
 const AdminPage = () => {
   const dispatch = useDispatch();
-  const { movies, loading, error } = useSelector((state) => state.movies);
+  const { movies, loading } = useSelector((state) => state.movies);
   const [editMovie, setEditMovie] = useState(null);
   const [formMovie, setFormMovie] = useState({
     name: "",
@@ -59,9 +59,6 @@ const AdminPage = () => {
   return (
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4 text-center">Chill Movie</h1>
-      {loading && <p>Loading...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-
       <form 
       className="w-90 lg:w-1/2 mx-auto"
       onSubmit={editMovie ? handleUpdate : handleCreate}>
@@ -128,11 +125,12 @@ const AdminPage = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="size-4 lg:size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
-                </button>
+                </button> 
                 </div>
               </td>
             </tr>
           ))}
+          {loading && <p>Loading...</p>}
         </tbody>
       </table>
     </div>
